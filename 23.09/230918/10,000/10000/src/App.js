74a0847aa2722a05c0e5376f76db4661e9b04ab8
geunpyochoi <ckp220@kookmin.ac.kr>
2023-09-18 16:50:59 +0900

    230918 10,000 hours update

---
+++ 23.09/230918/10,000/10000/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Components/header/header";
+import Main from "./Components/main/main";
+import Footer from "./Components/footer/footer";
+import Modal from "./Components/modal/modal";
+function App() {
+  return (
+    <div id="app">
+      <Header/>
+      <Main/>
+      <Footer/>
+      <Modal/>
+    </div>
+  );
+}
+export default App;
