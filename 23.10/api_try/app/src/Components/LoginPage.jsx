ccb53e53ddc4561ac249856e59e4468552f98f1a
geunpyochoi <ckp220@kookmin.ac.kr>
2023-10-06 14:20:43 +0900

    login update

--- 23.10/api_try/app/src/Components/LoginPage.jsx
+++ 23.10/api_try/app/src/Components/LoginPage.jsx
@@ -1,26 +1,52 @@
-import React, { useState } from 'react'
+import { useState } from "react"
 
-export default function LoginPage() {
+function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const inputEmail = (e) =>{
-    setEmail(e.target.value);
-  }
-  const inputPassword = (e) =>{
-    setPassword(e.target.value);
-  }
-  return (
-    <>
-      <h1>로그인</h1>
-      <section>
-        <h2>이메일, 비밀번호 입력하는 곳</h2>
-        <form>
-          <input type="text" placeholder='이메일 입력' onChange={inputEmail}/>
-          <input type="password" placeholder='비밀번호 입력' onChange={inputPassword}/>
-          <button type='submit'>로그인</button>
-        </form>
-      </section>
-    </>
-  )
+    const login = async (email,password)=>{
+        const baseUrl = "https://api.mandarin.weniv.co.kr";
+        const reqPath = "/user/login";
+        
+        const loginData = {
+          "user":{
+            "email":email,
+            "password":password
+          }
+        };
+        const reqUrl = baseUrl+reqPath;
+        fetch(reqUrl,{
+          method:"POST",
+          headers:{
+            "Content-type":"application/json"  
+          },
+          body:JSON.stringify(loginData)
+        });
+    }
+
+    const inputEmail = (e)=>{
+        setEmail(e.target.value);
+    }
+    const inputPassword = (e)=>{
+        setPassword(e.target.value);
+    }
+    const submitLogin = (e)=>{
+        e.preventDefault();
+        login();
+    }
+    return(
+        <>
+            <h1>로그인</h1>
+            <section>
+                <h2>이메일, 비밀번호 입력하는곳</h2>
+                <form onSubmit={submitLogin}>
+                    <input type="text" placeholder="이메일입력" onChange={inputEmail} value={email}/>
+                    <input type="text" placeholder="비밀번호입력" onChange={inputPassword} value={password}/>
+                    <button>로그인</button>
+                </form>
+            </section>
+        </>
+    )
 }
+
+export default LoginPage
